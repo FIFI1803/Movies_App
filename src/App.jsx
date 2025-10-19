ec08f61fc198bee97de69ff74ac2308c5fbbb7ca
Filip <filipgalach@icloud.com>
2025-10-19 18:34:15 +0100

    top 10 movies added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Spinner from './components/Spinner';
 import MovieCard from './components/MovieCard';
 import { useDebounce } from 'react-use';
 import { updateSearchCount } from './appwrite';
+import { getTrendingMovies } from './appwrite';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 
@@ -22,6 +23,7 @@ export default function App() {
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [debouncedSearchTerm, setDebouncedSearchTerm] = useState(searchTerm);
+  const [trendingMovies, setTrendingMovies] = useState([]);
 
   useDebounce(
     () => {
@@ -75,10 +77,24 @@ export default function App() {
     }
   };
 
+  const loadTrendingMovies = async () => {
+    try {
+      const movies = await getTrendingMovies();
+
+      setTrendingMovies(movies);
+    } catch (error) {
+      console.error('Error loading trending movies:', error);
+    }
+  }
+
   useEffect(() => {
     fetchMovies(debouncedSearchTerm);
 
   }, [debouncedSearchTerm]);
+
+  useEffect(() => {
+    loadTrendingMovies();
+  }, []);
 
   return (
     <main>
@@ -91,8 +107,25 @@ export default function App() {
           
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
+
+        {trendingMovies.length > 0 && (
+          <section className='trending'>
+            <h2>Trending Movies</h2>
+            <ul className='movie-list'>
+              {trendingMovies.map((movie, index) => (
+                  <li key={movie.$id}>
+                    <p>{index + 1}</p>
+                    <img src={movie.poster_url} alt={movie.title} />
+                  </li>
+              ))}
+            </ul>
+          </section>
+        )}
+
+
+
         <section className='all-movies'>
-        <h2 className='mt-[40px]'>All Movies</h2>
+          <h2>All Movies</h2>
 
 
         {isLoading ? (
